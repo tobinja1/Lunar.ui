425dce1827e17bd6afc00c951bcbc745b7963a58
Jay <77417138+tobinja1@users.noreply.github.com>
2024-09-16 10:50:50 -0400

    Integrate Hydra, center elements

---
+++ script.js
@@ -0,0 +1,17 @@
+var hydra = new Hydra({
+    canvas: document.getElementById("myCanvas"),
+    detectAudio: false,
+    enableStreamCapture: false,
+  })
+
+  noise(3,0.1,7)
+  .rotate(1,-1,-2).mask(shape(20))
+  .colorama(0.5)
+  .modulateScale(o0)
+  .modulateScale(o0,1,)
+  .blend(o0)
+  .blend(o0)
+  .blend(o0)
+  .blend(o0)
+  .out(o0)
+  
