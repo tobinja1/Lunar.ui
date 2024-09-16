5021f6ee0c55d84c6d3840ea61e6d15620170177
Jay <77417138+tobinja1@users.noreply.github.com>
2024-09-16 15:33:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ var hydra = new Hydra({
     enableStreamCapture: false,
   })
 
+let randOnInit = Math.random();
+
   noise(3,0.1,7)
-  .rotate(1,-1,-2).mask(shape(20))
-  .colorama(0.5)
+  .rotate(1,-1,-2).mask(shape(randOnInit * 20 + 1))
+  .colorama(randOnInit)
   .modulateScale(o0)
-  .modulateScale(o0,1,)
+  .modulateScale(o0,1.5,)
   .blend(o0)
   .blend(o0)
   .blend(o0)
