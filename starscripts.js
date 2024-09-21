7c49507abe6e941388cc8c4d854261e10663ea1b
Jay <77417138+tobinja1@users.noreply.github.com>
2024-09-21 12:38:57 -0400

    Add moon phase highlighting

--- starscripts.js
+++ starscripts.js
@@ -12,6 +12,8 @@ let center = 0;
 var distance = 0;
 let mouseIsDown = false;
 let lastDistance = 0;
+var normDistance = 0;
+var moonPhaseText;
 
 //hydra
 
@@ -36,13 +38,33 @@ const date = new Date();
 
 let day = date.getDate();
 let month = date.toLocaleString('default', { month: 'short' });
+let monthNum = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let fullDateline = year + "-" + monthNum + "-" + day;
 
 document.getElementById("knob-card-day").innerHTML = day;
 document.getElementById("knob-card-month").innerHTML = month;
 
-fetch("https://aa.usno.navy.mil/api/moon/phases/date?date=2024-9-20&nump=1")
+fetch("https://aa.usno.navy.mil/api/rstt/oneday?date=" + fullDateline + "%20&coords=41.89,%2012.48")
   .then((response) => response.json())
-  .then((response) => document.getElementById("knob-card-moonphase-text").innerHTML = response.phasedata[0].phase);
+  // .then((response) => document.getElementById("knob-card-moonphase-text").innerHTML = response.phasedata[0].phase);
+  .then((response) => document.getElementById("knob-card-moonphase-text").innerHTML = response.properties.data.curphase)
+
+moonPhaseText =  document.getElementById("knob-card-moonphase-text").innerHTML;
+
+if(moonPhaseText == "New Moon") {
+  document.getElementById("new-moon").style.opacity = 1;
+}
+else if(moonPhaseText == "Waxing Crescent" || moonPhaseText == "First Quarter" || moonPhaseText == "Waxing Gibbous") {
+  document.getElementById("waxing-moon").style.opacity = 1;
+}
+else if(moonPhaseText == "Full Moon") {
+  document.getElementById("full-moon").style.opacity = 1;
+}
+else {
+  document.getElementById("waning-moon").style.opacity = 1;
+}
 
 //knobs
 
@@ -59,6 +81,17 @@ function clamp(value, max, min) {
     if (mouseIsDown) {
       const newDistance = clamp(lastDistance + (center - pageY), 360, 0);
       distance = newDistance;
+      normDistance = distance/360;
+      knob.style.background = `conic-gradient(black 0deg, black ${distance}deg, white ${distance}deg 360deg)`;
+    }
+  }
+
+  function moveEndlessInteraction(pageY) {
+    if (mouseIsDown) {
+      const newDistance = clamp(lastDistance + (center - pageY), 360, 0);
+      distance = newDistance;
+      normDistance = distance/360;
+      console.log(normDistance);
       knob.style.background = `conic-gradient(black 0deg, black ${distance}deg, white ${distance}deg 360deg)`;
     }
   }
