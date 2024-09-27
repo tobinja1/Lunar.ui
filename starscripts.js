d8dff8cf7fd7c8f86b7f0de0827cf4c643003839
tobinja1 <tobinja1@gmail.com>
2024-09-27 16:38:20 -0400

    Responsive lunar phases

--- starscripts.js
+++ starscripts.js
@@ -16,6 +16,11 @@ let lastDistance = 0;
 var normDistance = 0;
 var moonPhaseText;
 
+var newMoonIcon = document.getElementById("new-moon");
+var waxingMoonIcon = document.getElementById("waxing-moon");
+var fullMoonIcon = document.getElementById("full-moon");
+var waningMoonIcon = document.getElementById("waning-moon");
+
 //hydra
 
 let randOnInit = Math.random();
@@ -52,20 +57,20 @@ fetch("https://aa.usno.navy.mil/api/rstt/oneday?date=" + fullDateline + "%20&coo
   // .then((response) => document.getElementById("knob-card-moonphase-text").innerHTML = response.phasedata[0].phase);
   .then((response) => document.getElementById("knob-card-moonphase-text").innerHTML = response.properties.data.curphase)
 
-moonPhaseText =  document.getElementById("knob-card-moonphase-text").innerHTML;
+// moonPhaseText =  document.getElementById("knob-card-moonphase-text").innerHTML;
 
-if(moonPhaseText == "New Moon") {
-  document.getElementById("new-moon").style.opacity = 1;
-}
-else if(moonPhaseText == "Waxing Crescent" || moonPhaseText == "First Quarter" || moonPhaseText == "Waxing Gibbous") {
-  document.getElementById("waxing-moon").style.opacity = 1;
-}
-else if(moonPhaseText == "Full Moon") {
-  document.getElementById("full-moon").style.opacity = 1;
-}
-else {
-  document.getElementById("waning-moon").style.opacity = 1;
-}
+// if(moonPhaseText == "New Moon") {
+//   document.getElementById("new-moon").style.opacity = 1;
+// }
+// else if(moonPhaseText == "Waxing Crescent" || moonPhaseText == "First Quarter" || moonPhaseText == "Waxing Gibbous") {
+//   document.getElementById("waxing-moon").style.opacity = 1;
+// }
+// else if(moonPhaseText == "Full Moon") {
+//   document.getElementById("full-moon").style.opacity = 1;
+// }
+// else {
+//   document.getElementById("waning-moon").style.opacity = 1;
+// }
 
 //knobs
 
@@ -83,6 +88,7 @@ function clamp(value, max, min) {
       const newDistance = clamp(lastDistance + (center - pageY), 360, 0);
       distance = newDistance;
       normDistance = distance/360;
+      moonIconCalc(normDistance)
       sunPercent.innerHTML = Math.floor(normDistance * 100) + "%";
       knob.style.background = `conic-gradient(black 0deg, black ${distance}deg, white ${distance}deg 360deg)`;
     }
@@ -104,15 +110,58 @@ function clamp(value, max, min) {
       lastDistance = distance;
     }
   }
+
+  function moonJump(degree){
+    distance = degree;
+    newNormDistance = distance/360;
+    knob.style.background = `conic-gradient(black 0deg, black ${distance}deg, white ${distance}deg 360deg)`;
+    sunPercent.innerHTML = Math.floor(newNormDistance * 100) + "%";
+    moonIconCalc(newNormDistance);
+    lastDistance = distance;
+  }
+
+  function moonIconCalc(degree){
+    if(degree >= .25 && degree < .50) {
+      document.getElementById("new-moon").style.opacity = 1;
+      document.getElementById("waxing-moon").style.opacity = 1;
+      document.getElementById("full-moon").style.opacity = 0.4;
+      document.getElementById("waning-moon").style.opacity = 0.4;
+    }
+    if(degree >= .50 && degree < .75) {
+      document.getElementById("new-moon").style.opacity = 1;
+      document.getElementById("waxing-moon").style.opacity = 1;
+      document.getElementById("full-moon").style.opacity = 1;
+      document.getElementById("waning-moon").style.opacity = 0.4;
+    }
+    if(degree >= .75 && degree < 1) {
+      document.getElementById("new-moon").style.opacity = 1;
+      document.getElementById("waxing-moon").style.opacity = 1;
+      document.getElementById("full-moon").style.opacity = 1;
+      document.getElementById("waning-moon").style.opacity = 1;
+    }
+    else if (degree < .25){
+      document.getElementById("new-moon").style.opacity = 1;
+      document.getElementById("waxing-moon").style.opacity = 0.4;
+      document.getElementById("full-moon").style.opacity = 0.4;
+      document.getElementById("waning-moon").style.opacity = 0.4;
+    }
+  }
   
   // Desktop events
   knob.addEventListener("mousedown", (e) => startInteraction(e.pageY));
   knob.addEventListener("mousedown", (e) => e.preventDefault());
 
+  newMoonIcon.addEventListener("mousedown", () => moonJump(0));
+  waxingMoonIcon.addEventListener("mousedown", () => moonJump(90));
+  fullMoonIcon.addEventListener("mousedown", () => moonJump(180));
+  waningMoonIcon.addEventListener("mousedown", () => moonJump(270));
+
   document.addEventListener("mousemove", (e) => moveInteraction(e.pageY));
   document.addEventListener("mousemove", (e) => e.preventDefault());
 
   document.addEventListener("mouseup", () => endInteraction());
+
+
   
   // Mobile events (mobile reacts to any touch at all)
   document.addEventListener("touchstart", (e) => {
@@ -124,5 +173,10 @@ function clamp(value, max, min) {
     const touch = e.touches[0];
     moveInteraction(touch.pageY);
   });
+
+  newMoonIcon.addEventListener("touchstart", () => moonJump(0));
+  waxingMoonIcon.addEventListener("touchstart", () => moonJump(90));
+  fullMoonIcon.addEventListener("touchstart", () => moonJump(180));
+  waningMoonIcon.addEventListener("touchstart", () => moonJump(270));
   
   document.addEventListener("touchend", () => endInteraction());
